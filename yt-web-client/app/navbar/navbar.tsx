ed749749fc48c7715c5bb8806484235aac5b5d5b
McFlanky <cturjanica02@gmail.com>
2024-04-18 12:46:32 -0400

    Added sign in component with ternary if/else or updating sign in to sign out, added use-client to navbar/sign-in for client side rendering, created navbar functionality with sign in button with useState/useEffect to add interactivity to update comp after its been rendered without having to reload page

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./navbar.module.css";
 
+import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+
+// closure to store the user state
 export default function Navbar() {
+    // Initialize user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    });
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <Image width={90} height={20}
                     src="/youtube-logo.svg" alt="Youtube Logo" />
             </Link>
+            {
+                //TODO: Add a upload button
+            }
+            <SignIn user={user} />
         </nav>
     );
 }
