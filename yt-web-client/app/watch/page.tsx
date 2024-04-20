dc7913290b887208e4e58c50a626ed9acef56b6b
McFlanky <cturjanica02@gmail.com>
2024-04-20 12:14:39 -0400

    Updated Link element in page.tsx to include the key prop and corrected the default caching behaviour by changing it from automatically caching every server side fetch request to only every 30 seconds... + created a dockerfile and built and pushed a docker image to Google Artifact Registry... + created next.config.js in root yt-web-client to stop the suspend useSearchParams error

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Watch() {
   const videoPrefix = 'https://storage.googleapis.com/mcf-yt-processed-videos/';
   const videoSrc = useSearchParams().get("v");
+  
   return (
     <div>
       <h1>Watch Page</h1>
-      <video controls src={videoPrefix + videoSrc} />
+        <video controls src={videoPrefix + videoSrc} />
     </div>
     );
 }
