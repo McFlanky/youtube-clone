bb0171ac41cc19654db7789f550f3b9a1bf93d0a
McFlanky <cturjanica02@gmail.com>
2024-04-20 11:23:16 -0400

    Added feature to show videos uploaded from anybody in web client on watch page.tsx, added getVideos function to functions.ts (haaave to initializeApp(firebaseConfig) is run before to avoid bug), Added thumbnail.png and fetched the videos within root page.tsx to render them as links, then finally added videos to watch page and conveted watch page to a client comp. which is required for useSearchParams in Next.js 13

--- yt-web-client/app/firebase/functions.ts
+++ yt-web-client/app/firebase/functions.ts
@@ -1,8 +1,17 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
-
-const functions = getFunctions();
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase.config";
 
 const generateUploadUrl = httpsCallable(functions, "generateUploadUrl");
+const getVideosFunction = httpsCallable(functions, "getVideos");
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: 'processing' | 'processed',
+  title?: string,
+  description?: string
+};
 
 export async function uploadVideo(file: File) {
     
@@ -21,3 +30,8 @@ export async function uploadVideo(file: File) {
 
   return;
 };
+
+export async function getVideos() {
+  const response = await getVideosFunction();
+  return response.data as Video[];
+}
